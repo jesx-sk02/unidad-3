8c775dd52abd783530b88b015c74f77229e24198
jesx-sk02 <gallegos.jm02@hotmail.com>
2024-10-21 12:34:02 -0500

    Add files via upload

---
+++ Practica 7/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Practica_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox2 = new GroupBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            Resultado = new TextBox();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            valor = new TextBox();
+            label1 = new Label();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.ForeColor = Color.MistyRose;
+            groupBox2.Location = new Point(12, 112);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(498, 107);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Opciones de Conversion";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightSteelBlue;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(187, 45);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 44);
+            button3.TabIndex = 6;
+            button3.Text = "BORRAR";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.RosyBrown;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(317, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 44);
+            button2.TabIndex = 5;
+            button2.Text = "CENTIGRADOS";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.RosyBrown;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(14, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 44);
+            button1.TabIndex = 4;
+            button1.Text = "FAHRENHEIT";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(Resultado);
+            groupBox3.Controls.Add(label2);
+            groupBox3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox3.ForeColor = Color.MistyRose;
+            groupBox3.Location = new Point(12, 245);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(498, 94);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Resultado en Grados";
+            // 
+            // Resultado
+            // 
+            Resultado.BackColor = Color.LavenderBlush;
+            Resultado.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Resultado.Location = new Point(207, 37);
+            Resultado.Name = "Resultado";
+            Resultado.Size = new Size(271, 30);
+            Resultado.TabIndex = 6;
+            Resultado.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaption;
+            label2.Location = new Point(14, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 23);
+            label2.TabIndex = 6;
+            label2.Text = "Equivalente en grados";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            groupBox1.Controls.Add(valor);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = Color.MistyRose;
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(498, 83);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // valor
+            // 
+            valor.BackColor = Color.LavenderBlush;
+            valor.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            valor.ForeColor = SystemColors.InactiveCaptionText;
+            valor.Location = new Point(187, 33);
+            valor.Name = "valor";
+            valor.Size = new Size(291, 30);
+            valor.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(35, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Ingresa valor";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(534, 369);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Conversiones";
+            TransparencyKey = Color.Transparent;
+            Load += Form1_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox2;
+        private Button button1;
+        private GroupBox groupBox3;
+        private GroupBox groupBox1;
+        private TextBox valor;
+        private Label label1;
+        private Button button3;
+        private Button button2;
+        private TextBox Resultado;
+        private Label label2;
+    }
+}
