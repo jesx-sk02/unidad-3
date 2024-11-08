e10a36fbc8922d888869929971c2cff2d7f59f64
Jesse Mariano <gallegos.jm02@hotmail.com>
2024-11-08 09:36:53 -0600

    Add files via upload

---
+++ Ejercicio 9/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Ejercicio_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(265, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Z = X^2 +X^3";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(76, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 28);
+            label2.TabIndex = 1;
+            label2.Text = "X";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(238, 82);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 28);
+            label3.TabIndex = 2;
+            label3.Text = "X^2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(409, 82);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 28);
+            label4.TabIndex = 3;
+            label4.Text = "X^3";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(597, 82);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 28);
+            label5.TabIndex = 4;
+            label5.Text = "Z";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkOrchid;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(202, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(296, 38);
+            button1.TabIndex = 5;
+            button1.Text = "CALCULAR VALORES";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(27, 113);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(125, 164);
+            listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(202, 113);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(125, 164);
+            listBox2.TabIndex = 7;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(375, 113);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(125, 164);
+            listBox3.TabIndex = 8;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.Location = new Point(549, 113);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(125, 164);
+            listBox4.TabIndex = 9;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Thistle;
+            ClientSize = new Size(716, 368);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
