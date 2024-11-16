ca5ff695d0927236dae576202ea1b3f55515f9df
Jesse Mariano <gallegos.jm02@hotmail.com>
2024-11-16 15:05:01 -0600

    Add files via upload

---
+++ Estructuras selectivas/Ejercicio 11/Ejercicio 16/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Ejercicio_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Long. lado A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Long. lado B";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 219);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Long. lado C";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(37, 54);
+            label4.Name = "label4";
+            label4.Size = new Size(148, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Diagrama triangulito";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(67, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 174);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(67, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(154, 212);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(67, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(37, 274);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 28);
+            button1.TabIndex = 7;
+            button1.Text = "Decision";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(265, 274);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 28);
+            button2.TabIndex = 8;
+            button2.Text = "Salir";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(154, 274);
+            button3.Name = "button3";
+            button3.Size = new Size(67, 28);
+            button3.TabIndex = 9;
+            button3.Text = "Borrar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 333);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Tipo de triangulo";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(176, 333);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(67, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label5;
+        private TextBox textBox4;
+    }
+}
