3c18d722fdf6ab99b43f84ca2153431fc2ef41e9
Jesse Mariano <gallegos.jm02@hotmail.com>
2024-11-16 15:15:16 -0600

    Add files via upload

---
+++ Estructuras repetitivas/Ejercicio 2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ejercicio_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(210, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(321, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Tabla de Multiplicacion";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(58, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(197, 50);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce el valor de \r\nla tabla de multiplicar";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(58, 271);
+            label3.Name = "label3";
+            label3.Size = new Size(206, 50);
+            label3.TabIndex = 2;
+            label3.Text = "Limite maximo para la \r\ntabla de multiplicación";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(437, 88);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 224);
+            listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MidnightBlue;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(437, 318);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 47);
+            button1.TabIndex = 4;
+            button1.Text = "CALCULAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ActiveCaption;
+            textBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(58, 154);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 34);
+            textBox1.TabIndex = 5;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ActiveCaption;
+            textBox2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(58, 331);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(214, 34);
+            textBox2.TabIndex = 6;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(663, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
